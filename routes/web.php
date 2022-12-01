ffce86749181070ceef64bcb74376f163fb24c47
binh.nguyenduc2000 <binh.nguyenduc2000@gmail.com>
2022-12-01 16:42:41 +0700

    Fix redirect home to login
    Người dùng chưa đăng nhập thì được redirect ra trang login

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+Route::group( ['middleware' => 'auth' ], function()
+{
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
